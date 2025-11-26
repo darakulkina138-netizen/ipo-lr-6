b4a0f2151e9d049362601b02b50e498e27f0779a
Даша <darakulkina138@gmail.com>
2025-11-26 11:33:28 +0300

    Update main.py

--- Задания/task_4/main.py
+++ Задания/task_4/main.py
@@ -1,1 +1,8 @@
-
+#Автор darakulkina138-netizen
+# Открываем файл и читаем его содержимое
+with open('text.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+# Разбиваем текст на слова (разделители: пробелы, переносы строк и т.д.)
+words = text.split()
+# Выводим количество слов
+print(f"Количество слов в файле: {len(words)}")
